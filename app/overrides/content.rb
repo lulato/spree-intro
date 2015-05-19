78489d038b6fd10ab4a29fc3a67d7aff6c88cd62
Luis La Torre <lulato@Luiss-iMac.local>
2015-05-19 17:24:40 -0400

    first commit

---
+++ app/overrides/content.rb
@@ -0,0 +1,4 @@
+# Deface::Override.new(:virtual_path  => "spree/home/index",
+#                      :replace => "[data-hook='homepage_products']",
+#                      :name          => "homepage_contents",
+#                      :partial => "home/homepage_contents")
